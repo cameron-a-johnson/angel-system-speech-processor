4285c489551b661dcb5d6d7cc6be6a99b348bf48
Derek Ahmed <derekhmd@gmail.com>
2023-06-21 01:24:51 -0400

    Add colored text printing to improve readability

--- speech_recognizer.py
+++ speech_recognizer.py
@@ -2,8 +2,8 @@ import subprocess
 import sys
 import time
 import whisper
+from termcolor import colored
 
-#PATH="/home/smg2280/Sayali-Workspace/espnet/egs2/aesrc2020/asr1/"
 class SpeechRecognizer:
 
     def __init__(self):
@@ -19,21 +19,11 @@ class SpeechRecognizer:
         audio_input = whisper.pad_or_trim(audio_input)
         mel = whisper.log_mel_spectrogram(audio_input).to(self.model.device)
 
-
         options = whisper.DecodingOptions(language="en")
         result = whisper.decode(self.model, mel, options)
-        text = result.text
+        colored_text = colored(result.text, "red")
         if debug_mode:
-            print(f"Transcript: {text}")
-
-        # #audio_input = str(sys.argv[1])
-        # audio_input = audio_file_path
-        
-        # process = subprocess.Popen(['bash',PATH+'run_single_audio.sh', audio_input])
-        # process.wait()
-        # with open(PATH+"exp/espnet/brianyan918_aesrc2020_asr_conformer/decode_asr_asr_model_valid.acc.ave/convert_me/text","r") as f:
-        #     text = " ".join(f.read().split(" ")[1:])
-        #     print("Transcript:", text)
+            print(f"Transcript: {colored_text}")
 
         print("Decoding took --- %s seconds ---" % (time.time() - start_time))
-        return text
+        return result.text
